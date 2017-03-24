7ce374e917a97de23e723b2a6818d7a15361a66a
niney <kpeteras@gmail.com>
2017-03-24 10:40:49 +0900

    ui-router 적용

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,12 +13,12 @@ angular
         'ngAnimate',
         'ngCookies',
         'ngResource',
-        'ngRoute',
         'ngSanitize',
-        'ngTouch'
+        'ngTouch',
+        'ui.router',
     ])
-    .config(function ($routeProvider) {
-        $routeProvider
+    .config(function ($stateProvider, $urlRouterProvider) {
+        /*$routeProvider
             .when('/', {
                 templateUrl: 'views/main.html',
                 controller: 'MainCtrl',
@@ -31,5 +31,24 @@ angular
             })
             .otherwise({
                 redirectTo: '/'
-            });
+            });*/
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('master', {
+                abstract: true,
+                templateUrl: 'views/t/master.html',
+                controller: 'MainCtrl as vm'
+            })
+            .state('master.home', {
+                url: '/',
+                templateUrl: 'views/main.html',
+                controller: 'HomeCtrl as vm'
+            })
+            .state('master.about', {
+                url: '/',
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl as vm'
+            })
     });
---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jwtNgApp.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the jwtNgApp
+ */
+angular.module('jwtNgApp')
+    .controller('HomeCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
